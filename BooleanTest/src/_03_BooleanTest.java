762930a47ba63bd84e9d1ec170b2206c6665eb5c
Lee SangHyo <leesanghyo2564@gmail.com>
2023-04-14 16:34:32 +0900

    불리언타입

---
+++ BooleanTest/src/_03_BooleanTest.java
@@ -0,0 +1,15 @@
+
+public class _03_BooleanTest {
+
+	public static void main(String[] args) {
+
+		//boolean bool0 = 50 > 20;
+		
+		int num1 = 100;
+		int num2 = 88;
+		
+		System.out.println(num1 & num2);
+		
+	}
+
+}
