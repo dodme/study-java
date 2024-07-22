dc13435cd18b448ea65cc40c006c1c442b7f781f
dodme <leesanghyo2564@gmail.com>
2024-07-22 17:55:11 +0900

    java복습

--- myProject/src/study3/test1.java
+++ myProject/src/study3/test1.java
@@ -31,6 +31,46 @@ public class test1 {
 
 		String[] split = subject.split(",");
 		System.out.println(split[1]);
+
+		String title = "자바프로그램 입문";
+		String content = "자바를 이용하여 완성한다.";
+
+		if (title.indexOf("자바") > -1 && content.indexOf("자바") > -1) {
+			System.out.println("둘다 존재함");
+		} else if (title.indexOf("자바") > -1 || content.indexOf("자바") > -1) {
+			System.out.println("둘중 한곳에만 존재");
+		} else {
+			System.out.println("둘다 존재하지 않음");
+		}
+
+		String score = "eng:70,kor:90,math:80";
+		String[] array = score.split(",");
+		// String[] array = {"eng:70", "kor:90", math:80"};
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+		String score2 = "eng:70,kor:90,math:80";
+		String[] array2 = score2.split(",");
+		// String[] array = {"eng:70", "kor:90", math:80"};
+		for (int i = 0; i < array2.length; i++) {
+			String[] str = array2[i].split(":");
+			System.out.print(str[1] + " ");
+		}
+
+		System.out.print("\n");
+
+		String str = "java";
+//		System.out.println("" + str.substring(0, 1));
+//		System.out.println(" " + str.substring(1, 2));
+//		System.out.println("  " + str.substring(2, 3));
+//		System.out.println("   " + str.substring(3, 4));
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			System.out.println(str.substring(i, i + 1));
+		}
 	}
 
 }
